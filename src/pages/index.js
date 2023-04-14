14580f398e0f2ba981858f81025228c548869729
Eran Fuchs <electranoob@outlook.com>
2023-04-14 11:31:13 +0300

    menu bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
     <>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Friends</a></li>
+        <li><a href="#">Feed</a></li>
+        <li><a href="#">News</a></li>
+      </ul>
     </>
   )
 }
